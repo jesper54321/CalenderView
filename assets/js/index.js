75df000c14fbede457050194893833c66685cc91
Hydrille <03rytter@gmail.com>
2021-10-09 10:03:37 +0200

    Sorting out code and adding day names

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,13 @@
 const calenderView = document.querySelector("#calenderView");
 let calenderControls = document.querySelectorAll("#calenderControl > a");
 let calenderText = document.querySelectorAll("#calenderControl > textarea");
-console.log(calenderText);
 
-let dateToday = new Date(Date.now());
-dateToday.setHours(0, 0, 0, 0);
-let chosenDate = dateToday;
+const dateToday = new Date(
+	new Date(Date.now()).getFullYear(),
+	new Date(Date.now()).getMonth(),
+	new Date(Date.now()).getDate()
+);
+let markedDate = dateToday;
 
 let firstOfMonth;
 
@@ -15,82 +17,109 @@ const getYear = function (dt) {
 const getMonth = function (dt) {
 	return new Date(dt).getMonth();
 };
+const getWeekday = function (dt) {
+	if (dt.getDay() == 0) {
+		return 6;
+	} else {
+		return dt.getDay() - 1;
+	}
+};
+
+function createNotificationBanner(tempDate) {
+	let notificationBanner = document.createElement("div");
+	notificationBanner.classList.add("notificationBanner");
+	notificationBanner.addEventListener("animationend", () => {
+		notificationBanner.remove();
+	});
+	notificationBanner.innerHTML =
+		"<p>This date has been marked!</p><p>" + tempDate + "</p>";
+	document.body.prepend(notificationBanner);
+}
+
+function dateOnClick(calenderDate, e, dateHolder) {
+	e.preventDefault();
+	let tempDate = dateHolder;
+	markedDate = tempDate;
+	if (document.querySelector("#calenderView .calenderPoint.active")) {
+		document
+			.querySelector("#calenderView .calenderPoint.active")
+			.classList.remove("active");
+	}
+	calenderDate.classList.add("active");
+	chooseNewDate(dateHolder);
+	createNotificationBanner(tempDate);
+}
+
+function createCalenderDate(i, dateHolder, year, month, weekday) {
+	let calenderDate;
+	if (
+		dateHolder.getDay() == 0 ||
+		dateHolder.getDay() == 6 ||
+		dateHolder < dateToday
+	) {
+		calenderDate = document.createElement("div");
+		calenderDate.classList.add("inactive");
+	} else {
+		calenderDate = document.createElement("a");
+		calenderDate.setAttribute("href", "#");
+		calenderDate.addEventListener("click", (e) => {
+			dateOnClick(calenderDate, e, dateHolder);
+		});
+		if (
+			dateHolder.getDate() == dateToday.getDate() &&
+			dateHolder.getMonth() == dateToday.getMonth()
+		) {
+			calenderDate.classList.add("today");
+		}
+		if (
+			dateHolder.getDate() == markedDate.getDate() &&
+			dateHolder.getMonth() == markedDate.getMonth()
+		) {
+			calenderDate.classList.add("active");
+		}
+	}
+	calenderDate.innerHTML = dateHolder.getDate();
+	calenderDate.classList.add("calenderPoint");
+	if (i < 7) {
+		let dOTW = document.createElement("p");
+		dOTW.innerHTML =
+			dateHolder
+				.toLocaleDateString("da-DK", { weekday: "short" })
+				.toUpperCase() + ".";
+		calenderDate.prepend(dOTW);
+	}
+	if (dateHolder.getDate() == 1) {
+		let dOTM = document.createElement("p");
+		dOTM.innerHTML = dateHolder
+			.toLocaleDateString("da-DK", { month: "short" })
+			.toLowerCase();
+		calenderDate.append(dOTM);
+	}
+	calenderView.appendChild(calenderDate);
+}
 
 function createCalenderView(dt) {
 	let year = getYear(dt);
 	let month = getMonth(dt);
-	let weekday;
+	let weekday = getWeekday(dt);
 
 	calenderView.innerHTML = "";
 
-	if (dt.getDay() == 0) {
-		weekday = 6;
-	} else {
-		weekday = dt.getDay() - 1;
-	}
 	for (let i = 0; i < 42; i++) {
 		let dateHolder = new Date(year, month, i + 1 - weekday);
-		let datePicker;
-		if (
-			dateHolder.getDay() == 0 ||
-			dateHolder.getDay() == 6 ||
-			dateHolder < dateToday
-		) {
-			datePicker = document.createElement("div");
-			datePicker.classList.add("inactive");
-		} else {
-			datePicker = document.createElement("a");
-			datePicker.setAttribute("href", "#");
-			datePicker.addEventListener("click", (e) => {
-				e.preventDefault();
-				let tempDate = dateHolder;
-				chosenDate = tempDate;
-				if (document.querySelector("#calenderView .calenderPoint.active")) {
-					document
-						.querySelector("#calenderView .calenderPoint.active")
-						.classList.remove("active");
-				}
-				datePicker.classList.add("active");
-				chooseNewDate(dateHolder);
-				let notificationBanner = document.createElement("div");
-				notificationBanner.classList.add("notificationBanner");
-				notificationBanner.addEventListener("animationend", () => {
-					notificationBanner.remove();
-				});
-				notificationBanner.innerHTML =
-					"<p>This date has been marked!</p><p>" + tempDate + "</p>";
-				document.body.prepend(notificationBanner);
-			});
-			if (
-				dateHolder.getDate() == dateToday.getDate() &&
-				dateHolder.getMonth() == dateToday.getMonth()
-			) {
-				datePicker.classList.add("today");
-			}
-			if (
-				dateHolder.getDate() == chosenDate.getDate() &&
-				dateHolder.getMonth() == chosenDate.getMonth()
-			) {
-				datePicker.classList.add("active");
-			}
-		}
-		datePicker.innerHTML = dateHolder.getDate();
-		datePicker.classList.add("calenderPoint");
-		calenderText[0].innerHTML = year;
-		calenderText[1].innerHTML = new Date(year, month, 1)
-			.toLocaleDateString("da-DK", { month: "long" })
-			.toUpperCase();
-		calenderView.appendChild(datePicker);
+
+		createCalenderDate(i, dateHolder, year, month, weekday);
 	}
+	calenderText[0].innerHTML = year;
+	calenderText[1].innerHTML = new Date(year, month, 1)
+		.toLocaleDateString("da-DK", { month: "long" })
+		.toUpperCase();
 }
 
 function chooseNewDate(dt) {
 	let year = getYear(dt);
 	let month = getMonth(dt);
 	firstOfMonth = new Date(year, month, 1);
-
-	console.log(firstOfMonth.toLocaleDateString("da-DK", { month: "long" }));
-	console.log(firstOfMonth.getFullYear());
 
 	createCalenderView(new Date(year, month, 1));
 }
@@ -100,9 +129,6 @@ function backwardMonth() {
 	let month = getMonth(firstOfMonth) - 1;
 	firstOfMonth = new Date(year, month, 1);
 
-	console.log(firstOfMonth.toLocaleDateString("da-DK", { month: "long" }));
-	console.log(firstOfMonth.getFullYear());
-
 	createCalenderView(new Date(year, month, 1));
 }
 
@@ -110,9 +136,6 @@ function forwardMonth() {
 	let year = getYear(firstOfMonth);
 	let month = getMonth(firstOfMonth) + 1;
 	firstOfMonth = new Date(year, month, 1);
-
-	console.log(firstOfMonth.toLocaleDateString("da-DK", { month: "long" }));
-	console.log(firstOfMonth.getFullYear());
 
 	createCalenderView(new Date(year, month, 1));
 }
@@ -122,9 +145,6 @@ function backwardYear() {
 	let month = getMonth(firstOfMonth);
 	firstOfMonth = new Date(year, month, 1);
 
-	console.log(firstOfMonth.toLocaleDateString("da-DK", { month: "long" }));
-	console.log(firstOfMonth.getFullYear());
-
 	createCalenderView(new Date(year, month, 1));
 }
 
@@ -133,9 +153,6 @@ function forwardYear() {
 	let month = getMonth(firstOfMonth);
 	firstOfMonth = new Date(year, month, 1);
 
-	console.log(firstOfMonth.toLocaleDateString("da-DK", { month: "long" }));
-	console.log(firstOfMonth.getFullYear());
-
 	createCalenderView(new Date(year, month, 1));
 }
 
@@ -143,7 +160,6 @@ calenderControls.forEach((element, i) => {
 	element.addEventListener("click", (e) => {
 		e.preventDefault();
 		let index = i;
-		console.log(index);
 		switch (index) {
 			case 0:
 				backwardYear();
